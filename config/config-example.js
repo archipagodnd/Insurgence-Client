184dc1477c4b7bed93abe196370278b6cc6ea6c4
Poilerwags <giestheuws@gmail.com>
2020-11-29 13:46:08 +0100

    first things to change :)

--- config/config-example.js
+++ config/config-example.js
@@ -19,7 +19,7 @@ Config.whitelist = [
 // address bar is `Config.routes.client`.
 Config.defaultserver = {
 	id: 'showdown',
-	host: 'sim3.psim.us',
+	host: '77.162.113.75',
 	port: 443,
 	httpport: 8000,
 	altport: 80,
