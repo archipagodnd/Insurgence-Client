a5e591466599dd381a710794dc264cbe900c02a4
Poilerwags <giestheuws@gmail.com>
2020-11-29 14:02:28 +0100

    Eh??

--- config/config.js
+++ config/config.js
@@ -32,3 +32,14 @@ Config.roomsFirstOpenScript = function () {
 Config.customcolors = {
 	'zarel': 'aeo'
 };
+/*** Begin automatically generated configuration ***/
+Config.version = "0.11.2 (7e4609c3)";
+
+Config.routes = {
+	root: 'pokemonshowdown.com',
+	client: 'play.pokemonshowdown.com',
+	dex: 'dex.pokemonshowdown.com',
+	replays: 'replay.pokemonshowdown.com',
+	users: 'pokemonshowdown.com/users',
+};
+/*** End automatically generated configuration ***/
