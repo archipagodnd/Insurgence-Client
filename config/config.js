4c0d5a6bcc750111faddf4775548aa6641260265
Poilerwags <55769238+Poilerwags@users.noreply.github.com>
2021-07-21 22:24:12 +0200

    this might be it

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (9d87e0c7)";
+Config.version = "0.11.2 (d67ae96b)";
 
 Config.routes = {
 	root: 'pokemonshowdown.com',
