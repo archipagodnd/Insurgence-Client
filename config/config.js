a69e5787c0e46190fb7fb870c89acd0e61b80b76
Poilerwags <giestheuws@gmail.com>
2022-08-13 19:37:16 +0200

    fdfae

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (45b6f7b8)";
+Config.version = "0.11.2 (d97859e4)";
 
 Config.routes = {
 	root: 'pokemonshowdown.com',
