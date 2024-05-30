184522e903a00a1cf7c20e434d275dfdcd0760e5
archipagodnd <148696839+archipagodnd@users.noreply.github.com>
2024-05-31 01:49:03 +0200

    togepi archi

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (bfc331ab)";
+Config.version = "0.11.2 (7d322f43)";
 
 Config.routes = {
 	root: 'pokemonshowdown.com',
